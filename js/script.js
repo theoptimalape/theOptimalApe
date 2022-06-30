59e851383ee3adfce69c89d9fbf2031286a764c5
Rodriguez <amador.rodriguez@zekelman.com>
2022-06-30 03:44:32 -0700

    Began carousel.

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Carousel
+const carousel = document.querySelector("#carousel");
+const slides = Array.from(carousel.children);
+const nextBtn = document.querySelector("#next-btn");
+const prevBtn = document.querySelector("#prev-btn");
+
+// const slideWidth = slides[0].getBoundingClientRect().width;
+
+nextBtn.addEventListener("click", (e) => {
+  const currentSlide = carousel.querySelector(".active");
+  const nextSlide = currentSlide.nextElementSibling;
+
+  currentSlide.classList.remove("active");
+  nextSlide.classList.add("active");
+});
+
+prevBtn.addEventListener("click", (e) => {
+  const currentSlide = carousel.querySelector(".active");
+  const prevSlide = currentSlide.previousElementSibling;
+
+  currentSlide.classList.remove("active");
+  prevSlide.classList.add("active");
+
+  if (currentSlide === slides[0]) {
+    prevBtn.style.display = none;
+  }
+});
