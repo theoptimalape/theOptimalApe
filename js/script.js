20c9dd84a1ec2f7e68be92e23dd573a8674c7b7a
Rodriguez <amador.rodriguez@zekelman.com>
2022-07-13 01:53:57 -0700

    prevBtn and nextBtn javaScript refined

--- js/script.js
+++ js/script.js
@@ -3,15 +3,26 @@ const carousel = document.querySelector("#carousel");
 const slides = Array.from(carousel.children);
 const nextBtn = document.querySelector("#next-btn");
 const prevBtn = document.querySelector("#prev-btn");
+const firstImg = document.querySelector("#img-1");
+const thirdImg = document.querySelector("#img-3");
 
-// const slideWidth = slides[0].getBoundingClientRect().width;
+if (firstImg.classList.contains("active")) {
+  prevBtn.style.display = "none";
+}
 
+// Event Listeners
 nextBtn.addEventListener("click", (e) => {
   const currentSlide = carousel.querySelector(".active");
   const nextSlide = currentSlide.nextElementSibling;
 
   currentSlide.classList.remove("active");
   nextSlide.classList.add("active");
+
+  prevBtn.style.display = "block";
+
+  if (thirdImg.classList.contains("active")) {
+    nextBtn.style.display = "none";
+  }
 });
 
 prevBtn.addEventListener("click", (e) => {
@@ -20,4 +31,10 @@ prevBtn.addEventListener("click", (e) => {
 
   currentSlide.classList.remove("active");
   prevSlide.classList.add("active");
+
+  nextBtn.style.display = "block";
+
+  if (firstImg.classList.contains("active")) {
+    prevBtn.style.display = "none";
+  }
 });
